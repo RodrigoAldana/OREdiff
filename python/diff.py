ee62e3d138ae0efd8f87bf30e9a5626a7a4295e9
Rodrigo Aldana-López <73191574+RodrigoAldana@users.noreply.github.com>
2024-07-18 12:17:56 -0600

    Update diff.py

--- python/diff.py
+++ python/diff.py
@@ -1,29 +1,17 @@
-# Copyright (C) 2022 Rodrigo Aldana-López
-# <rodrigo.aldana.lopez at {gmail,intel} dot com> (Intel Corporation)
-# For more information see <https://github.com/RodrigoAldana/OREdiff/blob/main/README.md>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Differentiator:
-    def __init__(self, kbar):
-        self.buffer = np.zeros(kbar + 1)
-        self.kbar = kbar
+    def __init__(self, L, Delta, Nbar, gamma=1.96):
+        self.L = L
+        self.Delta = Delta
+        self.Nbar = Nbar
+        self.gamma = gamma
+        self.kbar = int(np.ceil(np.sqrt(2 * Nbar / (L * Delta**2)) + 1))
+        self.buffer = np.zeros(self.kbar + 1)
+        self.y_filter = 0
 
-    def update_diff(self, f, k, Delta, L):
+    def update_diff(self, f, k):
         self.buffer[:-1] = self.buffer[1:]
         self.buffer[-1] = f
         Nhat = 0
@@ -31,56 +19,53 @@ class Differentiator:
         for l in range(2, min(k, self.kbar) + 1):
             for j in range(1, l + 1):
                 Q = self.buffer[-j] - self.buffer[-1] + (self.buffer[-1] - self.buffer[-l]) * (j / l)
-                Nlj = (abs(Q) - L * Delta**2 * j * (l - j) / 2) / 2
+                Nlj = (abs(Q) - self.L * self.Delta**2 * j * (l - j) / 2) / 2
                 if Nlj > Nhat:
                     Nhat = Nlj
 
         if Nhat == 0:
             lhat = 1
         else:
-            lhat = min(k, self.kbar, int(np.ceil(2 * np.sqrt(Nhat / L) / Delta)))
+            lhat = min(k, self.kbar, int(np.ceil(2 * np.sqrt(Nhat / self.L) / self.Delta)))
 
-        y = (self.buffer[-1] - self.buffer[-lhat]) / (lhat * Delta)
+        y = (self.buffer[-1] - self.buffer[-lhat]) / (lhat * self.Delta)
         return y, Nhat
 
-    def update_filter(self, y, input, Delta, gamma=1.96):
-        if abs(input - y) <= gamma * Delta:
-            sat = input - y
+    def update_filter(self, input):
+        if abs(input - self.y_filter) <= self.gamma * self.Delta:
+            sat = input - self.y_filter
         else:
-            sat = gamma * Delta * np.sign(input - y)
-        ynew = y + sat
-        return ynew
+            sat = self.gamma * self.Delta * np.sign(input - self.y_filter)
+        self.y_filter += sat
+        return self.y_filter
 
 if __name__ == '__main__':
     np.random.seed(0)
 
     Delta = 0.01
     L = 1
-    R = 1
     Nbar = 0.08
-    kbar = int(np.ceil(np.sqrt(2 * Nbar / (L * Delta**2)) + 1))
+    gamma = 1.96
 
     T = 20
     t = np.arange(0, T + Delta, Delta)
 
-    c = 0.5
     f = L * np.sin(t)
     df = L * np.cos(t)
 
     noise = 2 * Nbar * (np.random.rand(len(t)) - 0.5)
     y_diff = np.zeros(len(t))
     y_filter = np.zeros(len(t))
-    lhat = np.zeros(len(t))
     Nhat = np.zeros(len(t))
 
-    differentiator = Differentiator(kbar)
+    differentiator = Differentiator(L, Delta, Nbar, gamma)
 
     for k in range(1, len(t)):
         if k % 100 == 0:
             print(f"{k / len(t):.2%}")
         input_signal = f[k] + noise[k]
-        y_diff[k], Nhat[k] = differentiator.update_diff(input_signal, k, Delta, L)
-        y_filter[k] = differentiator.update_filter(y_filter[k-1], y_diff[k], Delta)
+        y_diff[k], Nhat[k] = differentiator.update_diff(input_signal, k)
+        y_filter[k] = differentiator.update_filter(y_diff[k])
 
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
